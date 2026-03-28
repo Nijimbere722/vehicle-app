5688d7230b54ceebaf572e293152145c7b580da9
unknown <nijimberemonia04@gmail.com>
2026-03-28 17:12:03 +0000

    working on api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './context/AuthoContext';
+import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
